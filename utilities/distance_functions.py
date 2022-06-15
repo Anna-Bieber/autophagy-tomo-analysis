2e20f33ebddfc5e344be8d168d8f68b855b41d08
Bieber <anna.bieber@yahoo.de>
2022-06-15 11:34:54 +0200

    Added example for analysis of an autophagic body still wrapped partially by the autophagosome/vacuole membrane.

--- utilities/distance_functions.py
+++ utilities/distance_functions.py
@@ -51,9 +51,9 @@ def minimum_distance_refined(a,b):
             - a_refined: points in a contributing to dist_refined
             - b_refined: points in b contributing to dist_refined
             - orig_dist_ab: original minimum distances a -> b
-            - orig_idx_ab: ids of a points closest to b points (original)
+            - orig_idx_ab: ids of b points closest to a points (original)
             - orig_dist_ba: original minimum distances b -> a
-            - orig_idx_ba: ids of b points closest to a points (original)
+            - orig_idx_ba: ids of a points closest to b points (original)
     """     
     # Determine KDTrees
     tree_a = cKDTree(a)
@@ -113,6 +113,66 @@ def minimum_distance_refined(a,b):
                                             'orig_dist_ba', 'orig_idx_ba'])
     results = result_tuple(dist_refined, a_refined, b_refined, 
                            dist_ab, idx_ab, dist_ba, idx_ba)
+    
+    return results
+
+def minimum_distance_cleaned(a,b):
+    """
+    Determine the minimum distances of points in a to points in b.
+    
+    Corresponds to first two steps of minimmum_distance_refined, leaving out the adding back procedure
+    and thus focusing exclusively on the minimum distance of a to b.
+      1. dist_ab: Minimum distances for each point in a to all points in b
+      2. Cleaning: check for points in b that were found by more than one point in a. 
+          Exclude all of the determined points in a except for the one with the smallest distance.
+
+    Parameters
+    ----------
+    a : numpy.ndarray
+        (n,3) array of xyz coordinates of points in group a.
+    b : numpy.ndarray
+        (n,3) array of xyz coordinates of points in group b.
+
+    Returns
+    -------
+    results : named tuple
+        Named tuple of results, containing the following numpy.ndarrays:
+            - dist_cleaned: cleaned minimum distances a -> b
+            - a_cleaned: points in a contributing to dist_cleaned
+            - b_cleaned: points in b contributing to dist_cleaned
+            - idx_ab_cleaned: ids of b points closest to cleaned a points
+            - orig_dist_ab: original minimum distances a -> b
+            - orig_idx_ab: ids of b points closest to a points (original)
+    """     
+    # Determine KDTree of b
+    tree_b = cKDTree(b)
+    # Find minimum distances a->b
+    dist_ab, idx_ab = tree_b.query(a)
+    # Check idx_ab for duplicates, giving unique values, the inverse indices for mapping them back, and the counts
+    unique, inverse_indices, counts = np.unique(idx_ab, return_inverse=True, return_counts=True) # returns unique indices, indices for mapping them back and counts of these unique indices
+    # Find indices in unique where counts are >1
+    unique_to_test = np.where(counts>1)[0] 
+
+    points_to_exclude = np.array([]) # initialize array
+    
+    # Determine the points to exclude
+    for idx_unique in unique_to_test: # loop over all indices that were found several times as nearest neighbours
+        points_to_test = np.where(inverse_indices==idx_unique)[0] # These are the indices of the points to test
+        idx_min = np.argmin(dist_ab[points_to_test]) # Find the one with the smallest distance
+        p_exclude = np.delete(points_to_test, idx_min) # Take out the point with the smallest distance
+        points_to_exclude = np.concatenate([points_to_exclude, p_exclude]) # All others are added to the list of points to exclude
+    
+    # Exclude the points from a, dist_ab and idx_ab
+    dist_ab_cleaned = np.delete(dist_ab, points_to_exclude.astype(np.int))
+    idx_ab_cleaned = np.delete(idx_ab, points_to_exclude.astype(np.int))
+    a_cleaned = np.delete(a, points_to_exclude.astype(np.int), axis=0)
+    b_cleaned = b[idx_ab_cleaned.astype(np.int)]
+    
+    # Define a named tuple to store results
+    result_tuple = namedtuple('distances', ['dist_cleaned', 'a_cleaned', 'b_cleaned', 'idx_ab_cleaned',
+                                            'orig_dist_ab', 'orig_idx_ab'])
+    results = result_tuple(dist_ab_cleaned, a_cleaned, b_cleaned, idx_ab_cleaned,
+                           dist_ab, idx_ab)
     
     return results
 
